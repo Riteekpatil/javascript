1513537c3c21594890b532273833c5080d133173
Riteekpatil <ritspatil555@gmail.com>
2023-05-08 15:57:38 +0530

    variableAssignment
    variableAssignment

---
+++ 02_variableBasicAssing.js
@@ -0,0 +1,50 @@
+console.log("<------------------- Number--------------->");
+
+var rollNumber= 51;
+console.log("Roll Number is:-",rollNumber,"||","Type is:-",typeof rollNumber);
+
+var id = 9834;
+console.log("Id  is:-",id ,"||","Type is:-",typeof id);
+
+var Phone_No= 8329070706;
+console.log("Phone No is:-",Phone_No,"||","Type is:-",typeof Phone_No);
+
+console.log("<------------------- String-------------->");
+  var firstName= "Riteek";
+  console.log("First Name is:-",firstName,"||","Type is:-",typeof firstName);
+
+  var lastName= "Patil";
+  console.log("Last Name is:-",lastName,"||","Type is:-", typeof lastName);
+
+  var address= "abcd";
+  console.log("Address is:-",address,"||","Type is:-",typeof address);
+
+  console.log("<---------------- Boolean-------------->");
+
+  var css=true;
+  console.log("Know About CSS:-",css,"||","Type is:-",typeof css);
+
+  var married=false;
+  console.log("Married:-",married,"||","Type is:-",typeof married);
+
+  var disability=false
+  console.log("Disability:-",disability,"||","Type is:-",typeof disability);
+
+
+  console.log("<<------------Diffrence Between Null nd Unefind------------>>");
+  console.log("***Null***");
+   console.log("Null means Variable is Declared but it's Value assigned Null");
+   var riteek = null;
+   console.log("value Of NUll is:-",riteek, "||","Type is:-", typeof riteek);
+
+
+   console.log("***Undefined***");
+   console.log("Undefined means Variable is Declared but it's Value not assigned ");
+   var abc;
+   console.log("value Of Unfined is:-",abc,"||", "Type is:-", typeof abc);
+
+
+
+
+
+
---
+++ 03_typeof.js
@@ -0,0 +1,40 @@
+   
+console.log("<<-----------Before------------->>");
+var bankName;
+console.log("Bank Name is:-",bankName,"||","Type is:-",typeof bankName);
+
+console.log("<<-----------After-------------->>");
+bankName= "SBI Bank";
+console.log("Bank Name is:-",bankName,"||","Type is:-",typeof bankName);
+
+console.log("<<<------------------*********--------------->>>>");
+var marks='90%';
+console.log("Marks is:-",marks,"Type is:-",typeof marks);
+
+
+console.log("<<-----------Before-------------->>");
+var isWorking;
+console.log(" isWorking:-",isWorking,"||","Value is:-", typeof isWorking);
+
+
+console.log("<<-----------After-------------->>");
+var isWorking;
+console.log("isWorking :-",isWorking);
+var isWorking=true;
+console.log( "isWorking:-",isWorking,"||","Value is:-", typeof isWorking);
+
+
+console.log("<<-------****************--------->>");
+var totalCount="Hundred nd Seven";
+console.log("Total Count is:-", totalCount,"||","Type is:-",typeof totalCount);
+
+
+
+
+console.log("<<-------Update--------->>");
+var totalCount=107;
+console.log("Total Count is:-", totalCount,"||","Type is:-",typeof totalCount);
+
+
+
+
