3d6745930700d26fd8be13233f4b33e32bd6c413
Riteekpatil <ritspatil555@gmail.com>
2023-05-08 15:57:00 +0530

    primitiveType
    primitiveType

---
+++ 01_primitiveTypes.js
@@ -0,0 +1,42 @@
+var age = 31;
+var weight = 78;
+var height = 6.1;
+var speckNumber = 0.50;
+console.log(age,weight,height,speckNumber);
+console.log("weight",weight);
+console.log(height);
+console.log(speckNumber);
+var typeOf = typeof age;
+console.log(typeOf);
+console.log(typeof weight);
+  
+
+//boolen-->true or false
+var isMarried = true;
+var knowAngular=true;
+var isBachelor =false;
+console.log(isMarried);
+console.log(typeof isMarried);
+
+
+
+//3.String -->Sequence of Character enclosed in ' 'or '""
+  var firstName ="Bill";
+  var position ='Microsoft co-founder';
+  console.log(firstName);
+  console.log("Type of Variable Position is :-",typeof firstName);
+  console.log(8+8);
+  
+
+
+// Special Data type 
+// 1.Undefined
+  var address;
+  console.log("Type of variable Address is :-",typeof address);
+
+
+// 2.null
+
+var pin =null;
+console.log("Value of variable is:-",pin);
+console.log("Type Of Variable is:-",typeof pin );
