8d7119e1c26e0816f4c446410f951b52f8fdb22f
Riteekpatil <ritspatil555@gmail.com>
2023-05-11 16:32:23 +0530

    swapping values
    swapping values

---
+++ 0_2swaoVariable.js
@@ -0,0 +1,25 @@
+var sweety = "sweety";
+var cutie = "cutie";
+
+console.log("<--------Swapping Two Values-------->");
+ console.log("Before Swapping :-", sweety,cutie);
+
+ var temp = sweety;
+     sweety = cutie;
+     cutie = temp;
+
+console.log("After Swapping :-", sweety,cutie);
+console.log("                                                         .                                            .");
+
+console.log("<--------Swapping Three Values-------->");
+var num1 =100;
+var num2 =200;
+var num3 =300;
+console.log("Before Swapping :-",num1,num2,num3);
+
+var temp = num1;
+    num1 = num2;
+    num2 = num3;
+    num3  =temp;
+    console.log("After Swapping :-",num1,num2,num3);
+
