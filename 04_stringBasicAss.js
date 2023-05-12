847a4c5001da1b0d17c8a64469ecde153274066a
Riteekpatil <ritspatil555@gmail.com>
2023-05-12 13:22:44 +0530

    stringAssigment
    stringAssigment

---
+++ 04_stringBasicAss.js
@@ -0,0 +1,20 @@
+
+
+function stringBasic(){
+    console.log("<<---------------******----------------->>");
+    console.log("My dream Company is :-  Infosys");
+    console.log("                                              ");
+
+    var hobbies1="Learning new thing";
+    var hobbies2="Playing Cricket";
+    var hobbies3="Reading Book";
+    console.log("My Hobbies:- 1.",hobbies1,"2",hobbies2,"3.",hobbies3);
+    console.log("                         ");
+    var l= hobbies1.length;
+    var le= hobbies2.length;
+    var len= hobbies3.length;
+    result=l+le+len;
+    console.log("Total Length Of Character is:-",result);
+
+}
+stringBasic();
---
+++ 04_stringHandtemplate.js
@@ -0,0 +1,31 @@
+
+
+function stringHandOn(){
+    console.log("<<-----------********-------->>");
+    console.log("    Hey you are doing good,keep it up    ");
+    console.log("                    ");
+    var abc= "    Hey you are doing good,keep it up    ";
+    var len=abc.length;
+    console.log("    The Length Of the String is:-",len);
+    console.log("                                   ");
+    console.log("<<--------------***************----------");
+    var triming= abc.trim();
+    var qwe= triming.length;
+    console.log("After leadig nd trailing, Length of String is:-",qwe);
+    console.log("                ");
+    var qwe= triming.length;
+    console.log("Total Number of Remove Extra space count :-",len-qwe);
+    console.log("                       ");
+
+     var tyuui= triming.charAt(0);
+     var bnm=triming.charAt(triming.length-1);
+     console.log("First Character is:-",tyuui,"||","Last Character is:-",bnm);
+  
+
+
+    //  var greet = "Good Morning";
+    //       var charAtZeroIndex = greet.charAt(0);
+    //   console.log("Char at 0 index: ",charAtZeroIndex);
+
+ }
+stringHandOn();
