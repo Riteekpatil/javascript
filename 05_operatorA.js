e7234396cf6ba08f3eb9a476d08675fc57d4cbcf
Riteekpatil <ritspatil555@gmail.com>
2023-05-15 16:56:28 +0530

    OperatoeAssignment
    OperatoeAssignment

---
+++ 05_operatorA.js
@@ -0,0 +1,27 @@
+
+console.log(`<<-------------No.1--------->>`);
+
+function squareOfWordLength(str){
+
+    var len= str.length;
+    console.log(`The length of the String is:-${len} And Square is:-${result=len*len}`);
+
+
+}
+squareOfWordLength("javaScript");
+squareOfWordLength("Google Chrome");
+squareOfWordLength("Developer Smart");
+
+console.log(`<<------------No.2---------->>`);
+
+function angular(){
+    var str="I am Angular Developer";
+    
+    console.log(`Given String:-${str}`);
+    console.log(`                         `);
+    var leng=str.length;
+    console.log(`The Length of the string is:-${leng} And Total no of Word Divide result is:-${res=22/4}`);
+    console.log(`                                                                                     `);
+    console.log(`The Length of the string is:-${leng} And Total no of Word Multiplication result is:-${res=22*4}`);
+}
+angular();
