73f1a5be0fbaf239883782d8e054715dd0190821
Riteekpatil <ritspatil555@gmail.com>
2023-05-27 17:35:51 +0530

    06_whileLoop_Assignment
    06_whileLoop_Assignment

---
+++ 06_while_Assignment.js
@@ -0,0 +1,114 @@
+var concat=" ";
+let i=5;
+while (i<=15) {
+    concat=concat+i+"  ";
+
+   
+    i++;
+   
+    
+}
+console.log(`1. WAP a program to print number from "5" to "15" by increment by 1`);
+console.log(`  ${concat}`);
+
+console.log(``);
+
+
+
+var conca=" ";
+let j=50;
+while (j>=40) {
+    conca=conca+j+"  ";
+    j--;
+    
+}
+console.log(`2. WAP a program to print number from "50" to "40" by decrement by 1`);
+console.log(`  ${conca}`);
+console.log(`                              `);
+console.log(`3. WAP to find first 15 odd number`);
+var concat=" ";
+let odd=0;
+while (odd<=30) {
+    if(odd%2!=0)  {    
+     concat=concat+odd+"  ";
+    }
+  
+    odd++;
+}
+console.log(`  ${concat}`);
+
+
+
+console.log(`                              `);
+console.log(`4. WAP to find first 10 Even number`);
+var count=" ";
+let even=0;
+while (even<=18) {
+    if (even%2==0) 
+    {
+       count=count+even+"  ";
+    }
+    even++;
+}
+console.log(`  ${count}`);
+
+console.log(`        `);
+
+
+
+
+
+
+console.log(`5. WAP to print table of 5  `);
+var con=" ";
+let five=5;
+while (five<=50) {
+ 
+    
+    con=con+five+"  ";
+    five=five+5;
+   
+    
+}
+
+console.log(`  ${con}`);
+
+
+
+
+
+
+
+ console.log(`        `);
+console.log(`6. WAP to print table of 10  `);
+    
+
+
+
+
+var sumee=" ";
+let ten=10;
+while (ten<=100) {
+ 
+    
+    sumee=sumee+ten+"  ";
+    ten=ten+10;
+   
+    
+}
+
+console.log(`  ${sumee}`);
+
+
+
+ console.log(`        `);
+ console.log(`7. WAP to print table of 10 reverse order `);
+  let hundredR=100;
+  var add=" ";
+ while (hundredR>=10) {
+
+    add=add+hundredR+"  ";
+    hundredR=hundredR-10;
+    
+ }
+ console.log(`  ${add}`);
