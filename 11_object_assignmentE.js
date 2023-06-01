201e011fc335dd2206f18412088ed9ed6b44bb21
Riteekpatil <ritspatil555@gmail.com>
2023-06-01 22:45:20 +0530

    11_objectAssignmentE
    11_objectAssignmentE

---
+++ 11_object_assignmentE.js
@@ -0,0 +1,80 @@
+let sbiBank={ 
+    bankName:"SBI Bank",
+    location:"PUNE",
+    accountNo:675438965,
+    ifsc:"SBI00986",
+    interestRate:"8.75%",
+
+     Show:function(){
+
+        let bankDeatils =`Bank Details: Bank Name: ${this.bankName},Location : ${this.location}, Account NO.: ${this.accountNo},IFSC Code: ${this.ifsc},Interest Rate: ${this.interestRate}`;
+        console.log(bankDeatils);
+
+
+     }
+
+};
+
+sbiBank.Show();
+///222
+
+
+let axisBank={ 
+    bankName:"Axis Bank",
+    location:"Mumbai",
+    accountNo:432338965,
+    ifsc:"AXIS000765",
+    interestRate:"9.71%",
+
+     Show:function(){
+
+        let bankDeatils =`Bank Details: Bank Name: ${this.bankName},Location : ${this.location}, Account NO.: ${this.accountNo},IFSC Code: ${this.ifsc},Interest Rate: ${this.interestRate}`;
+        console.log(bankDeatils);
+
+
+     }
+
+};
+
+axisBank.Show();
+
+///3
+
+
+let hdfcBank={ 
+    bankName:"HDFC Bank",
+    location:"Surat",
+    accountNo:8765434560,
+    ifsc:"HDFC00956",
+    interestRate:"7.80%",
+
+     Show:function(){
+
+        let bankDeatils =`Bank Details: Bank Name: ${this.bankName},Location : ${this.location}, Account NO.: ${this.accountNo},IFSC Code: ${this.ifsc},Interest Rate: ${this.interestRate}`;
+        console.log(bankDeatils);
+
+
+     }
+
+};
+
+hdfcBank.Show();
+
+let yesBank={ 
+    bankName:"YES Bank",
+    location:"Nashik",
+    accountNo:987896570,
+    ifsc:"YES00007",
+    interestRate:"8.30%",
+
+     Show:function(){
+
+        let bankDeatils =`Bank Details: Bank Name: ${this.bankName},Location : ${this.location},Account NO.: ${this.accountNo},IFSC Code: ${this.ifsc},Interest Rate: ${this.interestRate}`;
+        console.log(bankDeatils);
+
+
+     }
+
+};
+
+yesBank.Show();
